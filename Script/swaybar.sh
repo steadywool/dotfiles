a37219e59f13a6143c3600ee85aeb8d9c48babdd
Kaniville <kaniville17@tutanota.com>
2021-08-26 21:38:29 +0200

    Updated swaybar for adding a second audio card

---
+++ Script/swaybar.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Show date & hour
+date_formatted=$(date +"%a %d %b %Y %H:%M")
+
+# Show battery capacity
+battery_info=$(cat /sys/class/power_supply/BAT0/capacity)
+
+# Show volume and mute info
+volume=$(pactl list sinks | grep Volume | head -n1 | awk '{print $5}')
+mute=$(pactl list sinks | grep Mute | cut -c 7-)
+
+# Show contrast level with light
+contrast=$(light -G)
+
+# Show linux version
+linux_version=$(uname -r)
+
+# Echo command for sway-bar
+echo [VER $linux_version] [CONT $contrast%] [MUTE $mute] [VOL $volume] [BAT $battery_info%] [$date_formatted]
