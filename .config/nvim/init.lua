e6f3f76f3bf0245ce515d024ca61ae05e96d5aa0
Kaniville <kaniville18@tutamail.com>
2021-10-09 22:00:59 +0200

    I've forgotten a line for autocompletion

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -80,7 +80,9 @@ require'colorizer'.setup()
 require'lspinstall'.setup()
 local servers = require'lspinstall'.installed_servers()
 for _, server in pairs(servers) do
-    require'lspconfig'[server].setup{}
+    require'lspconfig'[server].setup{
+        capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
+    }
 end
 
 -- Autocompletion
