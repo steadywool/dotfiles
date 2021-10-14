2ff80d54bae6ada4e405938874b77d42f273671b
Kaniville <kaniville18@tutamail.com>
2021-10-14 11:33:54 +0200

    Added battery status to swaybar Because i want to see my battery status

--- Script/swaybar.sh
+++ Script/swaybar.sh
@@ -3,8 +3,15 @@
 # Show date & hour
 date_formatted=$(date +"%a %d %b %Y %H:%M")
 
-# Show battery capacity
-battery_info=$(cat /sys/class/power_supply/BAT0/capacity)
+# Show battery capacity & status
+bat_capacity=$(cat /sys/class/power_supply/BAT0/capacity)
+bat_status=$(cat /sys/class/power_supply/BAT0/status)
+
+if [ $bat_status = "Charging" ]; then
+    bat_info="↑"
+elif [ $bat_status = "Discharging" ]; then
+    bat_info="↓"
+fi
 
 # Show volume and mute info
 volume=$(pactl list sinks | grep Volume | head -n1 | awk '{print $5}')
@@ -17,4 +24,5 @@ contrast=$(light -G)
 linux_version=$(uname -r | cut -d '-' -f1)
 
 # Echo command for sway-bar
-echo [VER $linux_version] [CONT $contrast%] [MUTE $mute] [VOL $volume] [BAT $battery_info%] [$date_formatted]
+echo [VER $linux_version] [CONT $contrast%] [MUTE $mute] [VOL $volume] [BAT $bat_capacity% $bat_info] [$date_formatted]
+
