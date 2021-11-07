35da00a4f3e7fc9085d3286603ebe4e1edd0d041
Kaniville <kaniville18@tutamail.com>
2021-11-07 11:09:08 +0100

    Disabled Treesitter & added new telescope hotkey

--- .config/nvim/lua/keybinds.lua
+++ .config/nvim/lua/keybinds.lua
@@ -2,6 +2,7 @@
 vim.cmd('nnoremap <M-f> <cmd>Telescope find_files<CR>')
 vim.cmd('nnoremap <M-g> <cmd>Telescope live_grep<CR>')
 vim.cmd('nnoremap <M-b> <cmd>Telescope buffers<CR>')
+vim.cmd('nnoremap <M-h> <cmd>Telescope command_history<CR>')
 
 -- Go to next/previous buffer in buffer list
 vim.cmd('nnoremap <TAB> :bnext<CR>')
--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -1,12 +1,12 @@
-require'nvim-treesitter.configs'.setup {
-    highlight = {
-        enable = true,
-        additional_vim_regex_highlighting = true,
-    },
+--require'nvim-treesitter.configs'.setup {
+--    highlight = {
+--        enable = true,
+--        additional_vim_regex_highlighting = true,
+--    },
 --    incremental_selection = {
 --        enable = true
 --    },
 --    indent = {
 --        enable = true
 --    },
-}
+--}
