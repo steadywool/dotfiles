0b61557b12f8efbc4648d3dcc658777d6cd02e32
Kaniville <60816131+Kaniville@users.noreply.github.com>
2022-04-10 17:59:55 +0200

    Use another theme for my terminal

--- .config/nvim/lua/plugins/themes.lua
+++ .config/nvim/lua/plugins/themes.lua
@@ -1,9 +1,16 @@
--- Nordic Theme
-require('nordic').colorscheme({
-    underline_option = 'none',
-    italic = false,
-    italic_comments = true,
-    minimal_mode = false,
-    alternate_backgrounds = false
-})
+-- nordic
+--require('nordic').colorscheme({
+--    underline_option = 'none',
+--    italic = false,
+--    italic_comments = true,
+--    minimal_mode = false,
+--    alternate_backgrounds = false
+--})
 
+-- gruvbox-material
+vim.g['gruvbox_material_better_performance']=1
+vim.g['gruvbox_material_enable_bold']=1
+vim.g['gruvbox_material_menu_selection_background']='yellow'
+vim.g['gruvbox_material_diagnostic_virtual_text']='colored'
+
+vim.cmd("colorscheme gruvbox-material")
