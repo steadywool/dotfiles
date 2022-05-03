3eb90042cdb3d5ca5721861bc6907a0b44ed7363
Kaniville <60816131+Kaniville@users.noreply.github.com>
2022-05-03 09:48:49 +0200

    Use friendly-snippets

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -18,6 +18,7 @@ return require("packer").startup(function(use)
         -- Snippets
         "L3MON4D3/LuaSnip",
         "saadparwaiz1/cmp_luasnip",
+        "rafamadriz/friendly-snippets",
 
         -- Completion
         "hrsh7th/nvim-cmp",
--- .config/nvim/lua/plugins/completion.lua
+++ .config/nvim/lua/plugins/completion.lua
@@ -1,5 +1,8 @@
 -- Completion
+local luasnip = require("luasnip")
 local cmp = require("cmp")
+
+require("luasnip.loaders.from_vscode").lazy_load()
 
 require("cmp").setup {
     completion = {
@@ -10,12 +13,29 @@ require("cmp").setup {
             require("luasnip").lsp_expand(args.body)
         end,
     },
-    mapping = cmp.mapping.preset.insert {
+    mapping = {
+        -- Completion
         ["<C-b>"] = cmp.mapping.scroll_docs(-4),
         ["<C-f>"] = cmp.mapping.scroll_docs(4),
         ["<C-Space>"] = cmp.mapping.complete(),
         ["<C-e>"] = cmp.mapping.abort(),
         ["<CR>"] = cmp.mapping.confirm {select = true},
+
+        -- Snippets
+        ["<Tab>"] = cmp.mapping(function(fallback)
+            if cmp.visible() then cmp.select_next_item()
+            elseif luasnip.expandable() then  luasnip.expand()
+            elseif luasnip.expand_or_jumpable() then luasnip.expand_or_jump()
+            else fallback()
+            end
+        end, {"i", "s"}),
+
+        ["<S-Tab>"] = cmp.mapping(function(fallback)
+            if cmp.visible() then cmp.select_prev_item()
+            elseif luasnip.jumpable(-1) then luasnip.jump(-1)
+            else fallback()
+            end
+        end, {"i", "s"}),
     },
     sources = cmp.config.sources {
         {name = "nvim_lsp"},
