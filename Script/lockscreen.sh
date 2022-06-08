60a7a8ebec7d34f793efcf53daf600796ad1c492
Kaniville <60816131+Kaniville@users.noreply.github.com>
2022-06-08 21:17:36 +0200

    Updated lockscreen & wallpaper script (now the lockscreen use a pixelized screenshot of each monitor

--- Script/lockscreen.sh
+++ Script/lockscreen.sh
@@ -1,9 +1,30 @@
 #!/bin/bash
 
-# If there is no wallpaper, use a color
-if [[ ! -f /tmp/blurred_wallpaper.jpg ]]; then
+TMP_WALLPAPER=/tmp/Wallpaper
+LOCK_ARGS=""
+
+# If grim is not installed
+if [[ ! $(which grim) ]]; then
     swaylock -e -f -c 1d2021
+
+# Else if grim is installed
 else
-    # Use the blurred wallpaper
-    swaylock -e -f -i /tmp/blurred_wallpaper.jpg
+
+    # For each connected output, do this
+    for output in $(swaymsg -t get_outputs | jq -r '.[] | .name'); do
+
+        screenshot=$TMP_WALLPAPER/$output.jpg
+
+        # Take the screenshot of each outputs
+        grim -o $output $screenshot
+
+        # Pixelize each screenshots
+        convert $screenshot -scale 10% -scale 1000% $screenshot
+
+        # Add arguments of each output to a variable
+        LOCK_ARGS="$LOCK_ARGS --image $output:$screenshot"
+    done
+
+    # Lock
+    swaylock -e -f $LOCK_ARGS
 fi
--- Script/wallpaper.sh
+++ Script/wallpaper.sh
@@ -1,38 +1,37 @@
 #!/bin/bash
 
 WALLPAPER=$HOME/Pictures/Wallpaper
-
-route=$(ip route show | grep -o 'default')
+TMP_WALLPAPER=/tmp/Wallpaper
 
 # Create wallpaper folder if there is none
 if [[ ! -d $WALLPAPER ]]; then
     mkdir -p $WALLPAPER
 fi
 
+# Create tmp wallpaper folder if there is none
+if [[ ! -d $TMP_WALLPAPER ]]; then
+    mkdir -p $TMP_WALLPAPER
+fi
+
 # If there is NO wallpaper in $WALLPAPER
-if [[ -z `ls -A $WALLPAPER` ]]; then
+if [[ -z $(ls -A $WALLPAPER) ]]; then
 
     # If there is a connection
-    if [[ ! -z $route ]]; then
+    if [[ ! -z $(ip route show | grep -o 'default') ]]; then
 
         # Download a wallpaper
-        wget -O /tmp/wallpaper.jpg https://source.unsplash.com/1920x1080/?nature
-        swaymsg output "*" bg /tmp/wallpaper.jpg fill
+        wget -O $TMP_WALLPAPER/wallpaper.jpg https://source.unsplash.com/1920x1080/?nature
+        swaymsg output "*" bg $TMP_WALLPAPER/wallpaper.jpg fill
 
     # If there is NO connection
     else
-        swaymsg output "*" bg "#000000" solid_color
+        swaymsg output "*" bg "#1d2021" solid_color
     fi
 
 # If there is wallpaper(s) in $WALLPAPER
 else
     # Select a random wallpaper from $WALLPAPER directory
-    cp $(find $WALLPAPER -type f | shuf -n1) /tmp/wallpaper.jpg
+    cp $(find $WALLPAPER -type f | shuf -n1) $TMP_WALLPAPER/wallpaper.jpg
 
-    swaymsg output "*" bg /tmp/wallpaper.jpg fill
-fi
-
-# Blur the wallpaper for the screenlocker if there is a wallpaper
-if [[ -f /tmp/wallpaper.jpg ]]; then
-    convert -scale 5% -blur 0x2.5 -resize 1000% /tmp/wallpaper.jpg /tmp/blurred_wallpaper.jpg
+    swaymsg output "*" bg $TMP_WALLPAPER/wallpaper.jpg fill
 fi
