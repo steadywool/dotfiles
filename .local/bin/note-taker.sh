c37841bc93356688a4690efc64c068cec9798b72
Kaniville <60816131+Kaniville@users.noreply.github.com>
2022-06-21 22:10:53 +0200

    Use a note-taking script

---
+++ .local/bin/note-taker.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+NOTE=$HOME/Documents/Notes
+
+if [[ ! -d $NOTE ]]; then
+    mkdir -p $NOTE
+fi
+
+note_filename=$NOTE/$(date +%Y%m%d)_note.md
+
+if [[ ! -f $note_filename ]]; then
+    echo "# Note for $(date +%Y-%m-%d)" > $note_filename
+fi
+
+nvim -c "norm Go" \
+    -c "norm Go## $(date +%H:%M)" \
+    -c "norm G2o" \
+    -c "startinsert" $note_filename
