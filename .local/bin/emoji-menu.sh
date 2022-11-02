0a34fefb6ea383055ad1eba4272c64074034701f
Kaniville <60816131+Kaniville@users.noreply.github.com>
2022-11-02 22:14:42 +0100

    Added an emoji picker

---
+++ .local/bin/emoji-menu.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+EMOJI="$HOME/.local/bin/emoji-menu/emoji.txt"
+
+# If emoji.txt don't exist
+if [[ ! -f ${EMOJI} ]]; then
+    mkdir -p $(dirname ${EMOJI})
+    touch ${EMOJI}
+
+# Else if emoji.txt exist
+else
+
+    case ${1} in
+        # emoji-menu.sh --fetch
+        '--fetch')
+
+            # Our API key to emoji-api.com
+            api_key="cb99dbe6e9fceab25e156ea499b4323f40725a8a"
+            fetched_emoji=$(curl https://emoji-api.com/emojis?access_key=${api_key})
+
+            # If the command worked
+            if [[ ${?} -eq 0 ]]; then
+
+                # We select the emoji and its name only
+                fetched_emoji=$(echo ${fetched_emoji} | jq --raw-output ".[] | \"\(.character) \(.slug),\"")
+
+                # We write the result in our emoji list
+                echo ${fetched_emoji} | sed -e 's/, /\n/g' > ${EMOJI}
+            fi
+        ;;
+
+        # emoji-menu.sh
+        *)
+            # We copy the chosen emoji with Bemenu and Wl-clipboard
+            cat ${EMOJI} | bemenu -p "emoji" -l 16 --no-exec | cut -d ' ' -f1 | wl-copy
+        ;;
+    esac
+fi
