c394009d94feef30d5fd86ed835ed9132fe0bd39
kaniville <60816131+Kaniville@users.noreply.github.com>
2023-03-27 20:40:00 +0200

    Updated zsh completion

--- .config/zsh/completion.zsh
+++ .config/zsh/completion.zsh
@@ -8,10 +8,6 @@ zstyle ':completion:*' completer _extensions _complete _approximate
 zstyle ':completion:*' menu select
 zstyle ':completion:*' matcher-list 'm:{a-zA-Z}={A-Za-z}' 'r:|[._-]=* r:|=*' 'l:|=* r:|=*'
 
-zstyle ':completion:*' file-list all
-zstyle ':completion:*' list-colors ${(s.:.)LS_COLORS}
-
-
 zstyle ':completion:*' format '%B%F{blue}-- %d --%f%b'
 zstyle ':completion:*:corrections' format '%B%F{yellow}-- %d --%f%b'
 zstyle ':completion:*:warnings' format '%B%F{red}-- no matches found --%f%b'
