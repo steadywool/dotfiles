8175b9d5689bdd93e205ce13674c58b87585bd69
kaniville <60816131+Kaniville@users.noreply.github.com>
2023-03-27 08:57:45 +0200

    Updated screenshot menu

--- .local/bin/screenshot-menu.sh
+++ .local/bin/screenshot-menu.sh
@@ -3,12 +3,13 @@
 SCREENSHOT_DIR="${HOME}/Pictures/Screenshot"
 
 var+=('󰍹 save-screen')
-var+=('󰍹 copy-screen')
 var+=('󰹑 save-output')
-var+=('󰹑 copy-output')
 var+=(' save-active')
-var+=(' copy-active')
 var+=('󰒉 save-area')
+
+var+=('󰍹 copy-screen')
+var+=('󰹑 copy-output')
+var+=(' copy-active')
 var+=('󰒉 copy-area')
 
 CHOICE=$(printf '%s\n' ${var[@]} | fuzzel -d -l 8 --index)
@@ -18,28 +19,32 @@ if [[ ! -d ${SCREENSHOT_DIR} ]]; then
     mkdir -p ${SCREENSHOT_DIR}
 fi
 
-# Take a screenshot
+# Save screenshot
 case ${CHOICE} in
     0)
         /usr/share/sway/scripts/grimshot --notify save screen ${SCREENSHOT_DIR}/$(date +%s).png
     ;;
     1)
-        /usr/share/sway/scripts/grimshot --notify copy screen -
-    ;;
-    2)
         /usr/share/sway/scripts/grimshot --notify save output ${SCREENSHOT_DIR}/$(date +%s).png
     ;;
-    3)
-        /usr/share/sway/scripts/grimshot --notify copy output -
-    ;;
-    4)
+    2)
         /usr/share/sway/scripts/grimshot --notify save active ${SCREENSHOT_DIR}/$(date +%s).png
     ;;
+    3)
+        /usr/share/sway/scripts/grimshot --notify save area ${SCREENSHOT_DIR}/$(date +%s).png
+    ;;
+esac
+
+# Copy screenshot
+case ${CHOICE} in
+    4)
+        /usr/share/sway/scripts/grimshot --notify copy screen -
+    ;;
     5)
-        /usr/share/sway/scripts/grimshot --notify copy active -
+        /usr/share/sway/scripts/grimshot --notify copy output -
     ;;
     6)
-        /usr/share/sway/scripts/grimshot --notify save area ${SCREENSHOT_DIR}/$(date +%s).png
+        /usr/share/sway/scripts/grimshot --notify copy active -
     ;;
     7)
         /usr/share/sway/scripts/grimshot --notify copy area -
