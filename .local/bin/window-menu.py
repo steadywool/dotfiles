ef46daf9fd6f89c2bd984b65980d4500f65637ab
kaniville <60816131+Kaniville@users.noreply.github.com>
2023-03-31 21:10:18 +0200

    Updated version of the window-menu script that also show floating windows

--- .local/bin/window-menu.py
+++ .local/bin/window-menu.py
@@ -2,26 +2,63 @@
 
 from i3ipc import Connection
 import subprocess
+import re
 
-# Get the connection
 sway_connection = Connection()
 
-# Switch window based on ID
-def windows_list():
-    windows = sway_connection.get_tree().leaves() + sway_connection.get_tree().scratchpad().descendants()
+# Get every informations about windows
+def get_node_information():
+    tree = sway_connection.get_tree().descendants()
     focused = sway_connection.get_tree().find_focused()
+    node_information = []
 
-    window_information = [(focused.id, window.id, window.workspace().num, window.name) for window in windows]
-    window_string = '\n'.join(f'{" " if id == focus else " "}[{id}] [{num}] {name}' for focus, id, num, name in window_information)
+    # Check every nodes
+    for node in tree:
 
-    # Select window with menu
+        # Only get values from windows
+        if node.type == 'con' or node.type == 'floating_con':
+
+            # Variables
+            node_id = node.id
+            node_num = node.workspace().num
+            node_name = node.name
+            focus_id = focused.id
+
+            # Check wich node is the focused one
+            if node_id == focus_id: focus_char = " "
+            else: focus_char = " "
+
+            # Remove parents from the tuple and create strings
+            if node_name is not None: node_information.append(f'{focus_char} [{node_id}] [{node_num}] {node_name}')
+
+    return node_information
+
+# Select a value using a menu
+def menu_choice():
+    node_information = '\n'.join(get_node_information())
+
+    # Default menu
     menu_process = subprocess.Popen(['fuzzel', '-d', '-w 75'], stdin=subprocess.PIPE, stdout=subprocess.PIPE)
-    selected_window, _ = menu_process.communicate(input=window_string.encode())
-    selected_window = selected_window.decode().strip()
 
-    # Focus the chosen window
-    for _, id, _, _ in window_information:
-        if str(id) in selected_window:
-            sway_connection.command('[con_id="%s"] focus' % id)
+    # Select a node with the menu
+    selected_node, _ = menu_process.communicate(input=node_information.encode())
+    selected_node = selected_node.decode().strip()
 
-windows_list()
+    return selected_node
+
+# Focus the chosen window
+def switch_node():
+    selected_node = menu_choice()
+
+    # Get the value between the first two brackets
+    match = re.search(r'\[(.*?)\]', selected_node)
+
+    # Check if something have been matched
+    if match:
+        selected_id = match.group(1)
+        sway_connection.command('[con_id="%s"] focus' % selected_id)
+    else:
+        pass
+
+# Execute the program
+switch_node()
