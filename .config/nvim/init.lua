c846f1a2c3504daa1cbf7448d03e15c7627390de
kaniville <60816131+Kaniville@users.noreply.github.com>
2023-04-05 16:33:56 +0200

    Change LSP diagnostics sign

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,14 +1,18 @@
 -- Leader key
 vim.g.mapleader = ';'
 
--- Autocmds, functions & keymaps
-require('autocmds')
+-- Functions
 require('functions')
-require('keymaps')
 
 -- Plugins
 require('plugins')
-require_plugins()
+Require_plugins()
+
+-- Autocmds
+require('autocmds')
+
+-- Keymaps
+require('keymaps')
 
 -- Files
 vim.opt.swapfile = true
@@ -26,42 +30,32 @@ vim.opt.clipboard = 'unnamedplus'
 -- Mouse usage
 vim.opt.mouse = 'a'
 
--- Lines
---vim.opt.number = true
-vim.opt.relativenumber = true
-vim.opt.cursorline = true
-vim.opt.wrap = false
-
--- True color
+-- Colors
 vim.opt.termguicolors = true
 
--- Auto indentation
+-- Indentation
 vim.opt.autoindent = true
+
+-- Diagnostics
+Diagnostics_sign()
+
+-- Lines
+vim.opt.relativenumber = true
+vim.opt.number = true
+vim.opt.cursorline = true
+vim.opt.wrap = false
 
 -- Folding
 vim.opt.foldenable = true
 vim.opt.foldlevel = 99
 vim.opt.foldlevelstart = 99
---vim.opt.foldcolumn = '1'
---vim.opt.fillchars = { eob = ' ', fold = ' ', foldopen = '', foldsep = ' ', foldclose = '' }
 
 -- Tabs & spaces
 vim.opt.expandtab = true
 vim.opt.tabstop = 4
 vim.opt.shiftwidth = 4
-
 vim.opt.list = true
 vim.opt.listchars = { tab = '▶ ', trail = '•', nbsp = '␣' }
 
 -- Status
 vim.opt.statusline = '%#search# %n %#statusline# %F %r%m %= %w%y %#incsearch# %l:%c %p%% '
-
---[[
-vim.o.statuscolumn = ' %r %#FoldColumn#%{'
-.. 'foldlevel(v:lnum) > foldlevel(v:lnum - 1)' -- Check that the current line has a higher fold level than the previous one
-.. '? foldclosed(v:lnum) == -1' -- Check if the current line is a closed fold
-.. '? ""' -- If it is, show this icon
-.. ': ""' -- Else show this one
-.. ': " "' -- If the current line is not in a fold, display a blank space
-..'} '
---]]
