8bf0162889caecbaf0fa8a5ac1c1ba163fd7934b
kaniville <60816131+kaniville@users.noreply.github.com>
2023-04-21 22:08:38 +0200

    Updated wallpaper script

--- .local/bin/wallpaper-changer.sh
+++ .local/bin/wallpaper-changer.sh
@@ -9,13 +9,16 @@ fi
 case ${1} in
     # Use a random wallpaper from a folder
     '-r' | '--random')
-        gsettings set org.gnome.desktop.background picture-uri file://$(find ${2:-${WALLPAPER_DIR}} -type f | shuf -n1)
+        random_wallpaper=$(find ${2:-${WALLPAPER_DIR}} -type f | shuf -n1)
+        gsettings set org.gnome.desktop.background picture-uri file://${random_wallpaper}
+        gsettings set org.gnome.desktop.background picture-uri-dark file://${random_wallpaper}
     ;;
 
     #download a temporary wallpaper with Picsum
     '-d' | '--download')
         curl -L -o /tmp/wallpaper.jpg 'https://picsum.photos/1920/1080' && \
         gsettings set org.gnome.desktop.background picture-uri file:///tmp/wallpaper.jpg
+        gsettings set org.gnome.desktop.background picture-uri-dark file:///tmp/wallpaper.jpg
         swaymsg output '*' bg /tmp/wallpaper.jpg fill
     ;;
 esac
