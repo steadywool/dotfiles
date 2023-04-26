23ebc9ef1651b134a2166608e18762dd241b8388
kaniville <60816131+kaniville@users.noreply.github.com>
2023-04-26 13:16:30 +0200

    Only use FIsh as an interactive shell

--- .config/fish/conf.d/sway.fish
+++
@@ -1,5 +0,0 @@
-# Current TTY
-set TTY (tty)
-
-# Start Sway on the first tty
-[ $TTY = '/dev/tty1' ] && exec sway
--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,24 +1,18 @@
-# Always source aliases
-for i in (ls $__fish_config_dir/aliases/)
-    source $__fish_config_dir/aliases/$i
+if status is-interactive
+    # Remove fish greeting
+    set fish_greeting
+
+    # Enable vi keys
+    fish_vi_key_bindings
+
+    # Vi mode cursors
+    set fish_cursor_default block
+    set fish_cursor_insert line
+    set fish_cursor_replace_one underscore
+    set fish_cursor_visual block
+
+    # Always source aliases
+    for i in (ls $__fish_config_dir/aliases/)
+        source $__fish_config_dir/aliases/$i
+    end
 end
-
-# Remove fish greeting
-set fish_greeting
-
-# PATH variable
-set PATH $HOME/.local/bin $HOME/.local/share/flatpak/exports/bin $PATH
-
-# XDG variables
-set -Ux XDG_CONFIG_HOME $HOME/.config
-set -Ux XDG_CACHE_HOME $HOME/.cache
-set -Ux XDG_DATA_HOME $HOME/.local/share
-
-# Editor variables
-set -Ux EDITOR nvim
-set -Ux VISUAL nvim
-set -Ux PAGER less
-
-# Wayland variables
-set -x _JAVA_AWT_WM_NONREPARENTING 1
-set -x MOZ_ENABLE_WAYLAND 1
