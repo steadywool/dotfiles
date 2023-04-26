7d810fbe09761fab1ec0b5d636279adc51617eb5
kaniville <60816131+kaniville@users.noreply.github.com>
2023-04-26 14:32:54 +0200

    Replaced Bash with Fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,18 +1,46 @@
+# VARIABLES
+## PATH variable
+set PATH $HOME/.local/bin $PATH
+
+## XDG variables
+set -Ux XDG_CONFIG_HOME $HOME/.config
+set -Ux XDG_CACHE_HOME $HOME/.cache
+set -Ux XDG_DATA_HOME $HOME/.local/share
+
+## Editor variables
+set -Ux EDITOR nvim
+set -Ux VISUAL nvim
+set -Ux PAGER less
+
+## Wayland variables
+set -x _JAVA_AWT_WM_NONREPARENTING 1
+set -x MOZ_ENABLE_WAYLAND 1
+
+#-------------------------------------------------------------------
+# INTERACTIVE SHELL
 if status is-interactive
-    # Remove fish greeting
+    ## Remove fish greeting
     set fish_greeting
 
-    # Enable vi keys
+    ## Enable vi keys
     fish_vi_key_bindings
 
-    # Vi mode cursors
+    ## Vi mode cursors
     set fish_cursor_default block
     set fish_cursor_insert line
     set fish_cursor_replace_one underscore
     set fish_cursor_visual block
 
-    # Always source aliases
+    ## Always source aliases
     for i in (ls $__fish_config_dir/aliases/)
         source $__fish_config_dir/aliases/$i
     end
 end
+
+#-------------------------------------------------------------------
+# SWAYWM
+## Current TTY
+set TTY (tty)
+
+## Start Sway on the first tty
+[ $TTY = '/dev/tty1' ] && exec sway
