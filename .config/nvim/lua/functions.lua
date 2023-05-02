178a2b36e710f0e1393eaf75862e1326e40610c9
kaniville <60816131+kaniville@users.noreply.github.com>
2023-05-02 09:04:49 +0200

    Updated neovim function

--- .config/nvim/lua/functions.lua
+++ .config/nvim/lua/functions.lua
@@ -7,7 +7,7 @@ end
 
 -- Change diagnostics signs
 function Diagnostics_sign()
-    local signs = { Error = " ", Warn = " ", Hint = " ", Info = " " }
+    local signs = { Error = " ", Warn = " ", Hint = "󰌶", Info = " " }
     for type, icon in pairs(signs) do
         local hl = "DiagnosticSign" .. type
         vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
