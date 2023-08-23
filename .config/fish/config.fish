2553e7c9fe3efeb4e6fe8bca99605ea380b967d3
kaniville <60816131+kaniville@users.noreply.github.com>
2023-08-23 20:59:03 +0200

    Use Fish default key bindings

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -21,15 +21,6 @@ if status is-interactive
     ## Remove fish greeting
     set fish_greeting
 
-    ## Enable vi keys
-    fish_vi_key_bindings
-
-    ## Vi mode cursors
-    set fish_cursor_default block
-    set fish_cursor_insert line
-    set fish_cursor_replace_one underscore
-    set fish_cursor_visual block
-
     ## Always source aliases
     for i in (ls $__fish_config_dir/aliases/)
         source $__fish_config_dir/aliases/$i
